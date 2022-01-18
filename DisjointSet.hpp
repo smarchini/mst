506b3407f3cd7da4eafa93dfbe3670508e92872a
Stefano Marchini <pacman616@gmail.com>
2022-01-18 11:27:50 +0100

    Disjoint set (with ranking) implementation

---
+++ DisjointSet.hpp
@@ -0,0 +1,38 @@
+#pragma once
+
+#include <numeric>
+#include <vector>
+
+class DisjointSet {
+ private:
+  std::vector<size_t> parent, rank;
+
+ public:
+  DisjointSet(size_t size) : parent(size), rank(size) {
+    std::iota(std::begin(parent), std::end(parent), 0);
+  }
+
+  int find(size_t elt) {
+    if (parent[elt] == elt) return elt;
+    parent[elt] = find(parent[elt]);
+    return parent[elt];
+  }
+
+  bool unite(size_t a, size_t b) {
+    a = find(a);
+    b = find(b);
+    if (a == b) return false;
+    if (rank[a] < rank[b]) std::swap(a, b);
+    if (rank[a] == rank[b]) rank[a]++;
+    rank[a] += rank[b];
+    parent[b] = a;
+    return true;
+  }
+
+  size_t count() const {
+    size_t count = 0;
+    for (size_t i = 0; i < parent.size(); i++)
+      if (parent[i] == i) count++;
+    return count;
+  }
+};
