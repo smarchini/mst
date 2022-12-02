eea03ccedc074fa343f7549260ba66a586590ff1
Stefano Marchini <pacman616@gmail.com>
2022-12-02 02:05:38 +0100

    Tweaking

--- flake.nix
+++ flake.nix
@@ -1,10 +1,29 @@
 {
-  description = "MST & MSA";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/master";
+  };
 
-  inputs.flake-utils.url = "github:numtide/flake-utils";
+  outputs = { self, nixpkgs }:
+    let
+      pkgsFor = system: import nixpkgs { inherit system; };
+      targetSystems = [ "x86_64-linux" ];
+    in
+    {
+      devShells = nixpkgs.lib.genAttrs targetSystems (system:
+        let
+          pkgs = pkgsFor system;
+          mkShellNative = pkgs.mkShell.override { stdenv = pkgs.stdenvAdapters.impureUseNativeOptimizations pkgs.gcc12Stdenv; };
+        in
+        {
+          default = mkShellNative {
+            packages = with pkgs; [
+              gcc_latest
+              llvmPackages_latest.clang
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = nixpkgs.legacyPackages.${system};
-      in { devShell = import ./shell.nix { inherit pkgs; }; });
+              gtest
+              gbenchmark
+            ];
+          };
+        });
+    };
 }
